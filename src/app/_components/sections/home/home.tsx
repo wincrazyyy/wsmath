03af4a4cec93a630795d3decea8a21769a0e3340
Anthony Leung <anthonyleung07@gmail.com>
2025-12-18 14:52:22 +0000

    space for nav

--- src/app/_components/sections/home/home.tsx
+++ src/app/_components/sections/home/home.tsx
@@ -4,13 +4,12 @@ import { Hero } from "./hero";
 import { ProofPills } from "./proof-pills";
 
 export function Home() {
-
   return (
-    <>
+    <div className="pt-20">
       <SectionReveal>
         <Hero />
         <ProofPills />
       </SectionReveal>
-    </>
+    </div>
   );
 }
