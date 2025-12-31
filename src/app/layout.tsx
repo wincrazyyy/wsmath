385e5256a269c2aaf5fede8d72cc10be5dd8e4fc
Anthony Leung <anthonyleung07@gmail.com>
2025-12-31 11:59:12 +0000

    image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,42 @@ import "./globals.css";
 import { geistSans, geistMono, rubik } from "./_lib/fonts";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://wsmath.com"),
   title: "WSMath",
   description: "Professional Online Math Tutor",
+
+  openGraph: {
+    type: "website",
+    url: "https://wsmath.com/",
+    title: "WSMath",
+    description: "Professional Online Math Tutor",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "WSMath",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "WSMath",
+    description: "Professional Online Math Tutor",
+    images: ["/opengraph-image.png"],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body
